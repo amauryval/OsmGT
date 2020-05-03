ba50fd1cef01e7ca0a8d0ce9fc8f31062f8ba626
amaury5432 <amauryval@gmail.com>
2020-05-03 13:40:02 +0200

    upd (test_api_overpass) func and asserts

--- tests/test_api_overpass.py
+++ tests/test_api_overpass.py
@@ -6,10 +6,13 @@ from osmgt.compoments.core import OsmGtCore
 
 
 def overpass_query_result():
-    return OverpassApi(OsmGtCore().logger, "3600134383")
+
+    query = 'area(3600134383)->.searchArea;(way["highway"]["area"!~"."](area.searchArea););out geom;(._;>;);'
+    return OverpassApi(OsmGtCore().logger).querier(query)
 
 
 def test_api_overpass_railway_lines():
-    osm_data = overpass_query_result().data()
+    osm_data = overpass_query_result()
 
     assert len(osm_data) == 4
+    assert len(osm_data["elements"]) == 1543
