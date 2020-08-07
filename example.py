5cfd40c6444f864f65b5c3b725cde8c521e0aa3d
amaury5432 <amauryval@gmail.com>
2020-08-07 19:38:38 +0200

    upd (example.py)

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import geopandas
 from osmgt import OsmGt
 
 def test_from_web():
-    location = "roanne"
+    location = "lyon"
     poi_output_name = f"{location}_poi"
     network_output_name = f"{location}_network"
     bbox = (40.718087, -74.018433, 40.733356, -73.982749)
