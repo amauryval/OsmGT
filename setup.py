4de66724b94b873a974bfaea5d02ae5d587295f8
amaury5432 <amauryval@gmail.com>
2021-02-20 01:53:40 +0100

    upd (setup.py) new version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author="amauryval",
     author_email='amauryval@gmail.com',
     url="https://github.com/amauryval/osmgt",
-    version='0.7.4',
+    version='0.7.10',
     description="A library to play with OSM roads (and POIs) data using graph tool network library",
     entry_points={},
     install_requires=requirements,
