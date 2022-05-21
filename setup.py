524ccf46d6f55a3756abffd6275876aed5fa4e85
amauryval <amauryval@gmail.com>
2022-05-21 20:51:09 +0200

    upd (setup.py) lib reverted

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup, find_packages
 # used by meta.yaml, do not forget space
 requirements = [
     "geopandas >=0.10.2",
+    "requests >=2.26.0",
     "numpy >=1.21.3",
+    "geojson >=2.5.0",
+    "python-dateutil >=2.8.2",
     "graph-tool >=2.43",
     "cairo >=1.16.0",
     "scipy >=1.7.1",
     "more-itertools >=8.10.0",
+    "numba >=0.53.1",
     "requests-futures >=1.0.0",
-    "numba >=0.53.1"
+    "pygeos >=0.10.2"
 ]
 
 setup_requirements = []
@@ -20,7 +24,7 @@ setup(
     author="amauryval",
     author_email='amauryval@gmail.com',
     url="https://github.com/amauryval/osmgt",
-    version='0.8.10',
+    version='0.8.11',
     description="A library to play with OSM roads (and POIs) data using graph tool network library",
     entry_points={},
     install_requires=requirements,
