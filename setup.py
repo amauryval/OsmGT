6ea0da216fcd5c8ce666ea75a6930721cb3849d4
amauryval <36413727+amauryval@users.noreply.github.com>
2022-06-25 23:17:53 +0200

    try (*) new version p310 skip ci

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author="amauryval",
     author_email='amauryval@gmail.com',
     url="https://github.com/amauryval/osmgt",
-    version='0.8.12',
+    version='0.8.13',
     description="A library to play with OSM roads (and POIs) data using graph tool network library",
     entry_points={},
     install_requires=requirements,
